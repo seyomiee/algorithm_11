9fd1fc14526a68b153e2e1641532f1673cb495de
seyeon <tltptltptltp@gmail.com>
2020-09-28 14:35:54 +0900

    소수찾기

--- programmers/level01_19.cpp
+++ programmers/level01_19.cpp
@@ -1,4 +1,28 @@
 #include <iostream>
+#include<math.h>
+#include <memory.h>
+
+using namespace std;
+
+int solution(int n) {
+  bool* c = new bool[n + 1];
+  memset(c, 1, n+1);
+  int answer =0;
+  
+  for (int i = 2; i<= sqrt(n); i++){
+        for (int j = i + i; j <= n; j += i){
+          c[j]= false;
+        }
+    }for(int i=2;i<=n;i++){
+      if(c[i]==1) 
+       answer++;
+       }
+
+    delete[]c;
+    return answer;
+}
+/*
+#include <iostream>
 
 using namespace std;
 
@@ -9,3 +33,4 @@ int solution(int n) {
             answer++;
     }cout<<answer<<endl;
 }
+*/
