fdf55f7ae6054647357ee5dbfbf351925b50205b
seyeon <tltptltptltp@gmail.com>
2020-10-07 16:42:10 +0900

    최대공약수와최소공배수

--- programmers/level01_22.cpp
+++ programmers/level01_22.cpp
@@ -5,6 +5,13 @@ using namespace std;
 
 vector<int> solution(int n, int m) {
     vector<int> answer;
-    
+    int c = n > m ? m : n;
+	int d = n > m ? n : m;
+      while(d%c!=0){
+         int temp=c;
+         c=d%c;
+         d=temp;
+      } answer.push_back(c);
+        answer.push_back(m*n/c);
         return answer;
 }
