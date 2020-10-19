bd6e8e11d70af63e69556210b34011a2ec6450ca
seyeon <tltptltptltp@gmail.com>
2020-10-19 14:29:43 +0900

    크레인인형뽑기

--- programmers/level01_28.cpp
+++ programmers/level01_28.cpp
@@ -1,33 +1,26 @@
-#include<iostream>
-#include<vector>
-#include<algorithm>
+#include <string>
+#include <vector>
 
-using namespace  std;
+using namespace std;
 
-int main(){
- 
-    int answer=0;
-    vector<vector<int>> board={
-      {0,0,0,0,0},
-      {0,0,1,0,3},
-      {0,2,5,0,1},
-      {4,2,4,4,2},
-      {3,5,1,3,1}};
-     vector<int> moves= {1,5,3,5,1,2,1,4};
-
-     vector<int> basket;
-
+int solution(vector<vector<int>> board, vector<int> moves) {
+    int answer = 0;
+    vector<int> basket;
+    
     for(int m: moves){
-      int doll=board.at(m-1).back();
-      board.at(m-1).pop_back();
+      int n=0, doll=-1;
 
+      for(int n=0; n<board.size();n++){
+      doll=board.at(n).at(m-1);
+      board.at(n).at(m-1)=0;
+      if(doll!=0) break; 
+      }
       if(doll!=0){ 
         if(!basket.empty() && basket.back()==doll){
           answer++;
           basket.pop_back(); 
           } else basket.push_back(doll); 
       }else continue; 
-    }cout<<answer*2<<"answer";
+    }return answer*2;
      
 }
-//묹 잘못 읽어서 다시 풀어야 함 ㅎㅎㅎ;;;;;;
