f4d8fbc5a16556a19b6cc9fb984708c2ac8c7845
seyeon <tltptltptltp@gmail.com>
2020-10-22 22:57:17 +0900

    키패드

--- programmers/level01_29.cpp
+++ programmers/level01_29.cpp
@@ -1,65 +1,50 @@
-#include<iostream>
-#include<vector>
+#include <string>
+#include <vector>
 #include<math.h>
 
-using namespace  std;
+using namespace std;
 
-static int distance(int h, int n){
+static int x(int x){
+  if(x%3==2) return 2;
+  else if(x%3==1) return 1;
+  else return 3;
+}
+
+static int distance(float h, float n){
   if(n==0) n=11;
-  int h_x=h%3, h_y=ceil(h/3),
-    n_x=n%3, n_y=ceil(n/3);
+  int h_x=x(h) ,h_y=ceil(h/3),
+    n_x=x(n), n_y=ceil(n/3);
 
-  if()
 
   return (abs(h_x-n_x) + abs(h_y - n_y));
   
 }
-
-int main(){
- 
-    /*
-    */
-
-  string answer="";
-  vector<int> numbers= {1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5};
-  string hand= "right";
-  
-  int L=0,R=0;
-
-  for( int n : numbers ){
+string solution(vector<int> numbers, string hand) {
+    string answer = "";
+    int L=0,R=0;
+    for( int n : numbers ){
     if(n%3==1){
        answer+="L";
        L=n;
        }
-    //첫번째줄
     else if(n%3==0 && n!=0) {
       answer +="R";
       R=n;
-      }
-    //두번째줄
-    else {  //세번째줄
+      }else {
       int r= distance(R,n), l= distance(L,n);
-      cout << n <<endl;
-      cout<< R << " "<< r <<endl;
-      cout<< L << " "<< l <<endl;
       if(r==l){
         if(hand=="left"){
           answer +="L"; L=n;
-          cout <<"왼손 여기"<<endl;
         }else{
           answer+="R"; R=n;
-          cout <<"오른손"<<endl;
         }
-      }else if (r<l){
+        }else if (r<l){
         answer+="R";
         R=n;
-        cout <<"오른손"<<endl;
       }else if (r>l){
         answer+="L";
         L=n;
-        cout <<"왼손"<<endl;
       }
     }
-  }cout<<answer;
-     
+  }return answer;
 }
