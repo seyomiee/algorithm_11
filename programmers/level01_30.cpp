a52dc73071d1caf94946b09f5ef270d8ef88a874
seyeon <tltptltptltp@gmail.com>
2020-10-27 11:16:08 +0900

    다시

--- programmers/level01_30.cpp
+++ programmers/level01_30.cpp
@@ -11,35 +11,7 @@ int main(){
 
 string dartResult= "1D#2S*3S";
 int answer=0;
-char p='n'; //s a
- int n=0,m=0; 
-for(char c: dartResult){
- 
-  if(0<=c-48&&(int)c-48<=10){
-    n=c-48;
-  }else if(c=='S'){
-    answer+=n;
-  }
-  else if(c=='D') {
-    n*=n; 
-    answer+=n;
-  }else if (c=='T') {
-    n*=(n*n); 
-    answer+=n;
-  }else {
-    if(c=='*'){
-      if(p=='n') {n*=2; p='s'; }
-      else if(p=='s'){ n*=4; p='s';}
-      else if(p=='a'){ n*=-2; p='a';}
-    }else if(c=='#'){
-      n*=-1;
-      p='a';     
-  }
-      answer+=n;
-      n=0;
-    }
-  }
-//dkwlr
+
 
      
 }
